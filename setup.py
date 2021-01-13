32d5b7bcfdd1ace0c789e74b433b4b88d5e22b86
Abbe98 <albin.post@gmail.com>
2021-01-13 20:06:59 +0100

    replace requirements.txt with setup.py install_requires

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import setuptools
 
+requires = [
+    'requests==2.25.0',
+    'rdflib==5.0.0',
+    'minio==6.0.0',
+    'ipython==7.16.1',
+    'prefect==0.14.1',
+]
+
 setuptools.setup(
     name='giacore',
     version='0.1.0',
     py_modules=setuptools.find_packages(),
+    install_requires=requires,
     entry_points='''
         [console_scripts]
         giacore=giacore:giacore
